e3ab2045565da44efcd64cd5cefda89c5c5f0528
Emil Albrektsson <emil.albrektsson@nav.no>
2020-09-22 12:09:36 +0200

    Hent db-info fra env-vars

--- src/main/kotlin/no/nav/omsorgspenger/DataSourceBuilder.kt
+++ src/main/kotlin/no/nav/omsorgspenger/DataSourceBuilder.kt
@@ -9,9 +9,17 @@ internal class DataSourceBuilder(env: Map<String, String>) {
     private val databaseName = env["DATABASE_DATABASE"]
 
     private val hikariConfig = HikariConfig().apply {
-        jdbcUrl = env["DATABASE_URL"]
-        username = env["DATABASE_USERNAME"]
-        password = "debug" // env["DATABASE_PASSWORD"]
+        jdbcUrl = env["DATABASE_URL"] ?: String.format(
+                "jdbc:postgresql://%s:%s/%s%s",
+                requireNotNull(env["DATABASE_HOST"]) { "database host must be set if jdbc url is not provided" },
+                requireNotNull(env["DATABASE_PORT"]) { "database port must be set if jdbc url is not provided" },
+                requireNotNull(databaseName) { "database name must be set if jdbc url is not provided" },
+                env["DATABASE_USERNAME"]?.let { "?user=$it" } ?: "")
+
+        env["DATABASE_USERNAME"]?.let { this.username = it }
+        env["DATABASE_PASSWORD"]?.let { this.password = it }
+        this.password = "debug"
+
         maximumPoolSize = 3
         minimumIdle = 1
         idleTimeout = 10001
@@ -21,8 +29,8 @@ internal class DataSourceBuilder(env: Map<String, String>) {
     }
 
     init {
+
         checkNotNull(env["DATABASE_DATABASE"]) { "database must be set" }
-        checkNotNull(env["DATABASE_URL"]) { "jdbcUrl must be set" }
     }
 
     fun getDataSource(role: Role = Role.User): DataSource {
