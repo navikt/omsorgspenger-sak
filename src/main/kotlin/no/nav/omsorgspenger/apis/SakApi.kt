ec285a167b4ce8eba8765c15ba9d2c8e1307d376
Anders Kristiansen <58467024+andersnav@users.noreply.github.com>
2020-10-30 11:51:14 +0100

    Gjør sjekk mot tilgangsstyring (#17)
    * rest client + tester
    
    * requestfelter

--- src/main/kotlin/no/nav/omsorgspenger/apis/SakApi.kt
+++ src/main/kotlin/no/nav/omsorgspenger/apis/SakApi.kt
@@ -1,33 +1,51 @@
 package no.nav.omsorgspenger.apis
 
 import io.ktor.application.call
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.http.*
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
-import no.nav.omsorgspenger.client.pdl.Identitetsnummer
+import no.nav.omsorgspenger.client.TilgangsstyringRestClient
 import no.nav.omsorgspenger.sak.HentIdentPdlMediator
 import no.nav.omsorgspenger.sak.db.SaksnummerRepository
 
 data class HentSaksnummerRequestBody(
-        val identitetsnummer: String
+    val identitetsnummer: String
 )
 
 data class HentSaksnummerResponseBody(
-        val saksnummer: String
+    val saksnummer: String
 )
 
 internal fun Route.SakApi(
-        saksnummerRepository: SaksnummerRepository,
-        hentIdentPdlMediator: HentIdentPdlMediator
+    saksnummerRepository: SaksnummerRepository,
+    hentIdentPdlMediator: HentIdentPdlMediator,
+    tilgangsstyringRestClient: TilgangsstyringRestClient
 ) {
     post("/saksnummer") {
         val identitetsnummer = call.receive<HentSaksnummerRequestBody>().identitetsnummer
+
+        val jwt = call.principal<JWTPrincipal>().also {
+            if (it == null) {
+                return@post call.respond(HttpStatusCode.Unauthorized)
+            }
+        }
+
         val identer = setOf(identitetsnummer)
+        val beskrivelse = "slå opp saksnummer"
+        val harTilgangTilSaksnummer = tilgangsstyringRestClient.sjekkTilgang(identer, jwt.toString(), beskrivelse)
+
+        if (!harTilgangTilSaksnummer) {
+            return@post call.respond(HttpStatusCode.Forbidden)
+        }
+
         val saksnummer = saksnummerRepository.hentSaksnummer(identer)
-                ?: saksnummerRepository.hentSaksnummer(
-                        hentIdentPdlMediator.hentIdentitetsnummer(identer).getOrDefault(identitetsnummer, emptySet()))
+            ?: saksnummerRepository.hentSaksnummer(
+                hentIdentPdlMediator.hentIdentitetsnummer(identer).getOrDefault(identitetsnummer, emptySet())
+            )
 
         if (saksnummer != null)
             call.respond(HentSaksnummerResponseBody(saksnummer))
---
+++ src/main/kotlin/no/nav/omsorgspenger/client/TilgangsstyringRestClient.kt
@@ -0,0 +1,46 @@
+package no.nav.omsorgspenger.client
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import no.nav.omsorgspenger.config.Environment
+import no.nav.omsorgspenger.config.hentRequiredEnv
+import org.slf4j.LoggerFactory
+
+// TODO: health check
+internal class TilgangsstyringRestClient(
+    private val httpClient: HttpClient,
+    env: Environment
+) {
+
+    private val logger = LoggerFactory.getLogger(TilgangsstyringRestClient::class.java)
+    private val tilgangUrl = env.hentRequiredEnv("TILGANGSSTYRING_URL")
+
+    internal suspend fun sjekkTilgang(identer: Set<String>, jwt: String, beskrivelse: String): Boolean {
+        val response = httpClient.post<HttpStatement>("$tilgangUrl/api/tilgang/personer") {
+            header(HttpHeaders.Authorization, "Bearer $jwt")
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+            body = PersonerRequestBody(identer, Operasjon.Visning, beskrivelse)
+        }.execute()
+
+        return when (response.status) {
+            HttpStatusCode.NoContent -> true
+            HttpStatusCode.Forbidden -> false
+            else -> {
+                logger.error("Uventet response code ved tilgangssjekk", response.status, response.readText())
+                throw RuntimeException("Uventet response code (${response.status}) ved tilgangssjekk")
+            }
+        }
+    }
+}
+
+enum class Operasjon {
+    Visning
+}
+
+data class PersonerRequestBody(
+    val identitetsnummer: Set<String>,
+    val operasjon: Operasjon,
+    val beskrivelse: String
+)
