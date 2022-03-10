b8ff58b448e1b4363281e7af7dec1e5a3d136f94
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-03-10 09:00:39 +0100

    Dependency bumps (#108)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,16 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.8.2"
-val k9rapidVersion = "1.20220113090933-70694df"
+val k9rapidVersion = "1.20220215091703-a5f112c"
 val ktorVersion = "1.6.7"
-val dusseldorfKtorVersion = "3.1.6.7-1288df6"
+val dusseldorfKtorVersion = "3.1.6.7-f2a96e8"
 val jsonassertVersion = "1.5.0"
 
 // Database
-val flywayVersion = "8.4.3"
+val flywayVersion = "8.5.2"
 val hikariVersion = "5.0.1"
-val kotliqueryVersion = "1.6.1"
-val postgresVersion = "42.3.2"
+val kotliqueryVersion = "1.6.3"
+val postgresVersion = "42.3.3"
 val embeddedPostgres = "1.3.1"
 val embeddedPostgresBinaries = "12.9.0"
 
@@ -101,7 +101,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.3.3"
+        gradleVersion = "7.4"
     }
 
 }
