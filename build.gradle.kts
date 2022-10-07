66850586e87ce9ceb7984e2e4420116202dbbfd9
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-10-07 16:07:28 +0200

    Lagt til SLSA, dep bumps + gradle (#174)
    * Lagt til SLSA, dep bumps + gradle
    
    * Dep bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val junitJupiterVersion = "5.9.0"
-val k9rapidVersion = "1.20220711113850-0593e9e"
-val ktorVersion = "2.0.3"
-val dusseldorfKtorVersion = "3.2.0.3-d4fdef9"
+val junitJupiterVersion = "5.9.1"
+val k9rapidVersion = "1.20221007103720-75312fe"
+val ktorVersion = "2.1.2"
+val dusseldorfKtorVersion = "3.2.1.2-ce40a5b"
 val jsonassertVersion = "1.5.1"
 
 // Database
-val flywayVersion = "9.1.3"
+val flywayVersion = "9.3.0"
 val hikariVersion = "5.0.1"
 val kotliqueryVersion = "1.9.0"
-val postgresVersion = "42.4.1"
-val embeddedPostgres = "2.0.0"
+val postgresVersion = "42.5.0"
+val embeddedPostgres = "2.0.1"
 val embeddedPostgresBinaries = "12.9.0"
 
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -29,15 +29,19 @@ java {
 
 dependencies {
     implementation("no.nav.k9.rapid:river:$k9rapidVersion")
-    implementation("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
-    implementation("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
     implementation("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
     implementation("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
     implementation("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
     implementation("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
 
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-network-tls-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-network-tls-certificates-jvm:$ktorVersion")
 
     // Database
     implementation("com.zaxxer:HikariCP:$hikariVersion")
@@ -49,14 +53,12 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("io.zonky.test:embedded-postgres:$embeddedPostgres")
     testImplementation(platform("io.zonky.test.postgres:embedded-postgres-binaries-bom:$embeddedPostgresBinaries"))
+    testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testImplementation("org.skyscreamer:jsonassert:$jsonassertVersion")
     testImplementation("io.ktor:ktor-server-test-host-jvm:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-    testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion") {
-        exclude(group = "com.github.jknack")
-    }
-    testImplementation ("org.skyscreamer:jsonassert:$jsonassertVersion")
 }
 
 repositories {
@@ -103,7 +105,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.4.2"
+        gradleVersion = "7.5.1"
     }
 
 }
