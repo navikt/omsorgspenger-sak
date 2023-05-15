6b801272802a051ee3b182f0d0fb191d3305fa34
albrektsson <emil.albrektsson@nav.no>
2023-05-15 09:09:57 +0200

    Dep+gradle bumps, slettet snyk workflow, workflow for dep sub

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.9.2"
-val k9rapidVersion = "1.20230316100804-072b34b"
-val ktorVersion = "2.2.4"
-val dusseldorfKtorVersion = "3.2.2.4-98ccf55"
+val k9rapidVersion = "1.20230426095941-a725ff7"
+val ktorVersion = "2.3.0"
+val dusseldorfKtorVersion = "3.2.3.0-7a92774"
 val jsonassertVersion = "1.5.1"
 
 // Database
-val flywayVersion = "9.16.1"
+val flywayVersion = "9.17.0"
 val hikariVersion = "5.0.1"
 val kotliqueryVersion = "1.9.0"
 val postgresVersion = "42.6.0"
@@ -18,7 +18,7 @@ val embeddedPostgresBinaries = "12.9.0"
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.sonarqube") version "4.0.0.2929"
     jacoco
@@ -102,7 +102,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.0.2"
+        gradleVersion = "8.1.1"
     }
 
 }
