7cdb118deb998ff9f83c22758c7cbaba161c82bf
Helga.Nyrud <helga.nyrud@nav.no>
2024-11-14 12:34:54 +0100

    Nedgraderer ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val junitJupiterVersion = "5.11.3"
 val k9rapidVersion = "1.20240510083323-9f05ca1"
-val ktorVersion = "3.0.1"
-val dusseldorfKtorVersion = "6.0.0"
+val ktorVersion = "2.3.12"
+val dusseldorfKtorVersion = "5.0.10"
 val jsonassertVersion = "1.5.3"
 
 // Database
